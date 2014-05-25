d3576a979ec87c6cfb4a44dbb82fef3f578ecede
cowell21 <chrisrcowell@yahoo.com>
2014-05-25 13:35:47 -0700

    added helper method for time ago generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
+
   def comment_partial(comment, comments_by_parent)
     render(
       partial: "comments/comment",
       locals: { comment: comment, comments_by_parent: comments_by_parent }
       )
   end
+
+  def time_ago(created_time)
+    time_dif = Time.now.to_i - created_time.to_i
+    return " just now" if time_dif < 60
+    return " #{time_dif  / 60} mintues ago" if time_dif < 3600
+    return " #{time_dif / 3600} hours ago" if time_dif < 86400
+    " #{time_dif / 86400} days ago"
+  end
+
 end
