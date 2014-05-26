49670310b839bf85aefe49109e2811f9f519ed59
cowell21 <chrisrcowell@yahoo.com>
2014-05-26 09:45:35 -0700

    added slide up/down effects to comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,34 @@ class StaticPagesController < ApplicationController
   end
 
   def update
+    operation_add_aww
+    operation_add
+    redirect_to root_url
+  end
+
+  private
+  def operation_add_aww
+    data = JSON[open('http://www.reddit.com/r/aww/.json').read]
+
+    data["data"]["children"].each do |post|
+      if post["data"]["url"].length < 255 && post["data"]["url"].include?(".jpg") || post["data"]["url"].include?(".gif")
+        title = post["data"]["title"].slice(0..254)
+        url = post["data"]["url"]
+        Post.create( title: title, url: url, user_id: 2) unless Post.find_by_title(title)
+      end
+    end
+  end
+
+  def operation_add
+    data = JSON[open('http://www.reddit.com/.json').read]
+
+    data["data"]["children"].each do |post|
+      if post["data"]["url"].length < 255 && post["data"]["url"].include?(".jpg") || post["data"]["url"].include?(".gif")
+        title = post["data"]["title"].slice(0..254)
+        url = post["data"]["url"]
+        Post.create( title: title, url: url, user_id: 2) unless Post.find_by_title(title)
+      end
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,28 +15,4 @@ module ApplicationHelper
     " #{time_dif / 86400} days ago"
   end
 
-  def operation_add_aww
-    data = JSON[open('http://www.reddit.com/r/aww/.json').read]
-
-    data["data"]["children"].each do |post|
-      if post["data"]["url"].length < 255 && post["data"]["url"].include?(".jpg") || post["data"]["url"].include?(".gif")
-        title = post["data"]["title"].slice(0..254)
-        url = post["data"]["url"]
-        Post.create( title: title, url: url, user_id: 2) unless Post.find_by_title(title)
-      end
-    end
-  end
-
-  def operation_add
-    data = JSON[open('http://www.reddit.com/.json').read]
-
-    data["data"]["children"].each do |post|
-      if post["data"]["url"].length < 255 && post["data"]["url"].include?(".jpg") || post["data"]["url"].include?(".gif")
-        title = post["data"]["title"].slice(0..254)
-        url = post["data"]["url"]
-        Post.create( title: title, url: url, user_id: 2) unless Post.find_by_title(title)
-      end
-    end
-  end
-
 end
